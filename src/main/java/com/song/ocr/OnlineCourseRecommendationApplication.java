0b97626e61266dabbab9c1181734f1088d92ee18
Haimeng Song <haimengs@usc.edu>
2020-04-19 16:53:16 -0700

    initial commit

---
+++ src/main/java/com/song/ocr/OnlineCourseRecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.song.ocr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCourseRecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCourseRecommendationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/song/ocr/OnlineCourseRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.song.ocr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCourseRecommendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
